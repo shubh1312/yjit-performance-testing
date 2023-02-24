f7d74c601d8759c25e4e1f6a39521dc71ddc6a87
Shubham Gothi <shubham.gothi@box8.in>
2023-02-24 16:16:09 +0530

    adding simple endpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    def health
+        return render json: {status: 'Application is running', yjit_enabled: RubyVM::YJIT.enabled?.to_s}, status: 200
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  Rails.application.routes.draw do
+    get '/health', to: 'application#health'
+  end
 end
